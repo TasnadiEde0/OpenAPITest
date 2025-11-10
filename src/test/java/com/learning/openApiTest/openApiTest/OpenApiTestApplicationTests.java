ec90acf2f54fc814694226add81efdf8b1fffdb7
edetasnadi <tasnadiede0@gmail.com>
2025-11-10 13:44:53 +0200

    init

---
+++ src/test/java/com/learning/openApiTest/openApiTest/OpenApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.openApiTest.openApiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
